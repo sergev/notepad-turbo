c15a311e33e7b486f36274bf667f6099406ed97b
Serge Vakulenko <serge.vakulenko@gmail.com>
2026-05-02 19:08:56 -0700

    Change name to Notepad Turbo.

--- src/NNMenus.cpp
+++ src/NNMenus.cpp
@@ -1,8 +1,8 @@
 /*
- * This file is part of Notepad Next.
+ * This file is part of Notepad Turbo.
  * Copyright 2019 Justin Dailey
  *
- * Notepad Next is free software: you can redistribute it and/or modify
+ * Notepad Turbo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
--- src/SciIFaceTable.h
+++ src/SciIFaceTable.h
@@ -1,19 +1,19 @@
 /*
- * This file is part of Notepad Next.
+ * This file is part of Notepad Turbo.
  * Copyright 2019 Justin Dailey
  *
- * Notepad Next is free software: you can redistribute it and/or modify
+ * Notepad Turbo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Notepad Next is distributed in the hope that it will be useful,
+ * Notepad Turbo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Notepad Next.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Notepad Turbo.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 
