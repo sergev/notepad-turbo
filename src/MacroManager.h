d7b32cc485d822a1df6f2af86cc3a9564a7e2d6d
Serge Vakulenko <serge.vakulenko@gmail.com>
2026-05-02 21:27:02 -0700

    Add copyright to source files.

--- src/MacroManager.h
+++ src/MacroManager.h
@@ -1,6 +1,7 @@
 /*
  * This file is part of Notepad Turbo.
  * Copyright 2024 Justin Dailey
+ * Copyright 2026 Serge Vakulenko
  *
  * Notepad Turbo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
